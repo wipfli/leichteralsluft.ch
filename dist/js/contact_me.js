4c55bb6bc004e4c3c611bdf695e4a5577180245a
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2022-07-20 18:33:19 +0200

    Add GmbH blog

---
+++ dist/js/contact_me.js
@@ -0,0 +1,80 @@
+$(function() {
+
+    $(".contact-form-input").jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function($form, event, errors) {
+            // additional error messages or events
+        },
+        submitSuccess: function($form, event) {
+            event.preventDefault(); // prevent default submit behaviour
+            // get values from FORM
+            var name = $("input#name").val();
+            var email = $("input#email").val();
+            var phone = $("input#phone").val();
+            var message = $("textarea#message").val();
+            var firstName = name; // For Success/Failure Message
+            // Check for white space in name for Success/Fail message
+            if (firstName.indexOf(' ') >= 0) {
+                firstName = name.split(' ').slice(0, -1).join(' ');
+            }
+          
+            $("#buttonContactForm").html('Mitteilung Senden <i class="fa fa-spinner my-spin"></i>');  
+          
+            $.ajax({
+                url: "././mail/contact_me.php",
+                type: "POST",
+                data: {
+                    name: name,
+                    phone: phone,
+                    email: email,
+                    message: message
+                },
+                cache: false,
+                success: function() {
+                    // Success message
+                    $('#success').html("<div class='alert alert-success'>");
+                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-success')
+                        .append("<strong>Danke! Mitteilung wurde erfolgreich gesendet.</strong>");
+                    $('#success > .alert-success')
+                        .append('</div>');
+
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                    
+                    $("#buttonContactForm").html('Mitteilung Senden');
+                },
+                error: function() {
+                    // Fail message
+                    $('#success').html("<div class='alert alert-danger'>");
+                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-danger').append("<strong>Mitteilung konnte leider nicht gesendet werden. Bitte kontaktiere uns telefonisch unter 062 877 16 10.");
+                    $('#success > .alert-danger').append('</div>');
+                  
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                  
+                    $("#buttonContactForm").html('Mitteilung Senden');
+                },
+            })
+            
+            
+        },
+        filter: function() {
+            return $(this).is(":visible");
+        },
+    });
+
+    $("a[data-toggle=\"tab\"]").click(function(e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+});
+
+
+/*When clicking on Full hide fail/success boxes */
+$('#name').focus(function() {
+    $('#success').html('');
+});
